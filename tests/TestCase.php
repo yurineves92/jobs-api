c2399233c23c59a173fa9e6b8d6d22cc60d10163
Yuri Neves <yuri.neves@quadritech.com.br>
2021-10-19 18:11:06 -0300

    update for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,6 @@ abstract class TestCase extends BaseTestCase
      */
     public function createApplication()
     {
-        parent::setUp();
         return require __DIR__.'/../bootstrap/app.php';
     }
 }
